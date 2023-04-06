9ae5be60bf6bbc443cf8adf6c1dd9480f49e52fe
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2023-04-06 19:00:21 -0400

    exec9

--- JavaScript/Exec-09 Calculadora Geométrica/index.js
+++ JavaScript/Exec-09 Calculadora Geométrica/index.js
@@ -1,6 +1,6 @@
 function areaTriangulo() {
   base = prompt("Informe a base do triângulo:");
-  altura = prompt("Informe a base do altura:");
+  altura = prompt("Informe a altura:");
   resultado = (base * altura) / 2;
   alert("A area do triângulo é: " + resultado);
   return resultado;
@@ -16,20 +16,27 @@ function AreaRetangulo() {
 
 function AreaQuadrado() {
   let lado = prompt("Informe o lado do quadrado: ");
+  let resultado = lado * lado;
+  alert("A area do Quadrado é igual a: " + resultado);
 }
 
 function AreaTrapezio() {
-  let baseMaior = prompt("Informe a Base Maior do Trapézio");
-  let baseMenor = prompt("Informe a Base Menor do Trapézio");
+  let baseMaior = Number(prompt("Informe a Base Maior do Trapézio"));
+  let baseMenor = Number(prompt("Informe a Base Menor do Trapézio"));
   let altura = prompt("Informe a Altura do Trapézio");
+  let resultado = ((baseMaior + baseMenor) * altura) / 2;
+  alert(resultado);
 }
 
 function AreaCirculo() {
   let raio = prompt("Informe o raio do circulo: ");
   let pi = 3.14;
+  let resultado = pi * (raio * raio);
+  alert(resultado);
 }
-do {
-  opcao = prompt(
+
+function exibirMenu() {
+  return prompt(
     "Qual das opções abaixo deseja calcular?\n" +
       "\n1-Calcular a area do triângulo" +
       "\n2-Calcular a area do triângulo" +
@@ -38,25 +45,37 @@ do {
       "\n5-Calcular a area do triângulo" +
       "\n6-Sair"
   );
+}
 
-  switch (opcao) {
-    case "1":
-      areaTriangulo();
-      break;
-    case "2":
-      AreaRetangulo();
-      break;
-    case "3":
-      break;
-    case "4":
-      break;
-    case "5":
-      break;
-    case "6":
-      break;
+function executar() {
+  opcao = "";
+  do {
+    opcao = exibirMenu();
+    switch (opcao) {
+      case "1":
+        areaTriangulo();
+        break;
+      case "2":
+        AreaRetangulo();
+        break;
+      case "3":
+        AreaQuadrado();
+        break;
+      case "4":
+        AreaTrapezio();
+        break;
+      case "5":
+        AreaCirculo();
+        break;
+      case "6":
+        alert("Saindo...");
+        break;
 
-    default:
-      alert("Opção Inválida!");
-      break;
-  }
-} while (opcao != "6");
+      default:
+        alert("Opção Inválida!");
+        break;
+    }
+  } while (opcao != "6");
+}
+
+exibirMenu();
