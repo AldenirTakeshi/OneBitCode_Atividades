a1e05481244298fd48453862b178d6ce3a03f921
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2023-05-03 19:26:48 -0400

    Manipulando Estilos com o DOM

--- JavaScrip DOM/Cadastro de devs/index.js
+++ JavaScrip DOM/Cadastro de devs/index.js
@@ -5,12 +5,34 @@ function createLabel(text, htmlFor) {
   return label;
 }
 
-function createInput(id, value, name, typr = "text", placeholder = "") {
+function createInput(id, value, name, type = "text", placeholder = "") {
   const input = document.createElement("input");
   input.id = id;
   input.value = value;
   input.name = name;
-  input.type = text;
+  input.type = type;
   input.placeholder = placeholder;
   return input;
 }
+
+const addTechBtn = document.getElementById("addTechBtn");
+const devForm = document.getElementById("devForm");
+const developers = [];
+let inputRows = 0;
+
+addTechBtn.addEventListener("click", function (ev) {
+  ev.preventDefault();
+  const stackInputs = document.getElementById("stackInputs");
+
+  const newRow = document.createElement("li");
+  const rowIndex = inputRows;
+  inputRows++;
+  newRow.id = "inputRow-" + rowIndex;
+  newRow.className = "InputRow";
+
+  const techNameLabel = createLabel("Nome: ", "techName-" + rowIndex);
+  const techNameInput = createInput("techName-" + rowIndex, null, "techName");
+
+  stackInputs.append(newRow);
+  newRow.append(techNameLabel, techNameInput);
+});
