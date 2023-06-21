4b5e21238028ff1023f6310834584964388ac0f3
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2023-06-21 18:38:56 -0400

    loaders

--- JavaScript Moderno/Usando o webpack/webpack.config.js
+++ JavaScript Moderno/Usando o webpack/webpack.config.js
@@ -1,13 +1,14 @@
-const path = require("path");
-
 module.exports = {
   entry: {
     index: "./src/index.js",
-    hello: "./src/hello.js",
   },
   mode: "development",
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "[name].bundle.min.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
